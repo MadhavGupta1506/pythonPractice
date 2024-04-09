d171a33de3bb452fba6baa2a485ae7764c43749e
MadhavGupta1506 <gmadhav857@gmail.com>
2024-04-09 16:59:43 +0530

    CartesianProduct

---
+++ CartesianProduct.py
@@ -0,0 +1,8 @@
+a=list(map(int,input().split()))
+b=list(map(int,input().split()))
+cartesian=[]
+for i in range(len(a)):
+    for j in range(len(b)):
+        cartesian.append((a[i],b[j]))
+for i in cartesian:
+    print(iend="")
